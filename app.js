1596d35e8fb527ccf1bc05ed36b43a6ad818d12c
Moe Chaker <moechaker@Moes-MacBook-Pro.local>
2021-03-21 17:45:11 -0400

    Ready to attempt Deploying 1st try

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let express = require("express");
 let app = express();
 
+app.use("/styles",express.static(__dirname + "/styles"));
+app.use("/scripts",express.static(__dirname + "/scripts"));
+app.use("/images",express.static(__dirname + "/images"));
+app.use("/files",express.static(__dirname + "/files"));
+
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
